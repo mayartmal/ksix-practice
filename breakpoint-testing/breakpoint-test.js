3189b8ae74b3a22092b7d1e728cf9d7b42a9e899
mayartmal <mayartmal@gmail.com>
2025-06-03 17:46:07 +0400

    soak and bp tests was added

---
+++ breakpoint-testing/breakpoint-test.js
@@ -0,0 +1,23 @@
+import http from 'k6/http'
+import { sleep } from 'k6'
+
+const BASE_URL = "https://quickpizza.grafana.com";
+const ROOT = "/test.k6.io"
+const NEWS = "/news.php"
+const CONTACTS = "/contacts.php"
+
+export const options = {
+    stages: [
+        {
+            duration: '2h',
+            target: 10000 
+        }
+    ]
+
+    
+}
+
+export default function () {
+    http.get(BASE_URL + ROOT);
+    sleep(1);
+}
